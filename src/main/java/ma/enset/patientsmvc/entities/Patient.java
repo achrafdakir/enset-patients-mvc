15a729c9999df1c9eafd86c02211b60a0aeed75a
Achraf Dakir <achraf01dakir@gmail.com>
2024-05-09 02:39:06 +0100

    more changes

--- src/main/java/ma/enset/patientsmvc/entities/Patient.java
+++ src/main/java/ma/enset/patientsmvc/entities/Patient.java
@@ -1,6 +1,9 @@
 package ma.enset.patientsmvc.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,11 +15,14 @@ import java.util.Date;
 public class Patient {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotEmpty
+    @Size(min = 4, max = 40)
     private String nom;
     @Temporal(TemporalType.DATE)
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date dateNaissance;
     private boolean malade;
+    @DecimalMin("100")
     private int score;
 
 }
